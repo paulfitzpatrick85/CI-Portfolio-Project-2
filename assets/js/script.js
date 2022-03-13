a8b68714b1b7dc76eaf45515db0a652d9d235a7c
paulfitzpatrick85 <fitzer99@gmail.com>
2022-03-13 21:31:43 +0000

    all 3 sets of macros plus and minus buttons working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,25 @@
+
+//functions to add and subtract users clicked input
 function addP() {
-    document.getElementById('addMacros').value++;
+    document.getElementById('add-protein').value++;
 }
 
 function subP() {
-    document.getElementById('addMacros').value--;
+    document.getElementById('add-protein').value--;
+}
+
+function addC() {
+    document.getElementById('add-carb').value++;
+}
+
+function subC() {
+    document.getElementById('add-carb').value--;
+}
+
+function addF() {
+    document.getElementById('add-fat').value++;
+}
+
+function subF() {
+    document.getElementById('add-fat').value--;
 }
